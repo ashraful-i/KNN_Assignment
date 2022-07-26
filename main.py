fc27fb28dab94ee0c023dcd8e4161c6b1f1166c1
ashraful-i <ashraful.mahi@gmail.com>
2022-07-27 00:42:10 +0600

    assignment

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ def Euclidean_distance(row1, row2):
         distance += (row1[i] - row2[i]) ** 2  # (x1-x2)**2+(y1-y2)**2
     return sqrt(distance)
 
+
 def get_row_val(test_rows):
     print(len(test_rows))
     r = 0
@@ -27,14 +28,15 @@ def get_row_val(test_rows):
     r = r / len(test_rows)
     g = g / len(test_rows)
     b = b / len(test_rows)
-        #print(neb)
-    #print (r, g, b)
+    # print(neb)
+    # print (r, g, b)
     tr = list()
     tr.append(r)
     tr.append(g)
     tr.append(b)
     print(tr)
     return tr
+
 
 def GetTestAreas(train, test_row1, test_row2):
     print("GN")
@@ -45,8 +47,8 @@ def GetTestAreas(train, test_row1, test_row2):
     for i in train:
         dist1 = Euclidean_distance(test_row1, i)
         dist2 = Euclidean_distance(test_row2, i)
-        #print("dist1 = " + str(dist1) + " dist2 = " + str(dist2))
-        if(dist1<dist2):
+        # print("dist1 = " + str(dist1) + " dist2 = " + str(dist2))
+        if (dist1 < dist2):
             distance1.append(dist1)
             data1.append(i)
         else:
@@ -66,7 +68,7 @@ def GetTestAreas(train, test_row1, test_row2):
     global test_r_2
     test_r_1 = get_row_val(data1)
     test_r_2 = get_row_val(data2)
-    diff1 = abs(test_r_1[0] - test_row1[0])+abs(test_r_1[1] - test_row1[1])+abs(test_r_1[2] - test_row1[2])
+    diff1 = abs(test_r_1[0] - test_row1[0]) + abs(test_r_1[1] - test_row1[1]) + abs(test_r_1[2] - test_row1[2])
     diff2 = abs(test_r_2[0] - test_row2[0]) + abs(test_r_2[1] - test_row2[1]) + abs(test_r_2[2] - test_row2[2])
     if diff1 < 1 and diff2 < 1:
         print("diff < 1")
@@ -74,10 +76,60 @@ def GetTestAreas(train, test_row1, test_row2):
         return
     print("End")
     GetTestAreas(train, test_r_1, test_r_2)
-    #print(data1)
-    #print(data2)
+    # print(data1)
+    # print(data2)
 
-    return data1
+
+
+
+def GetTestAreas3(train, test_row1, test_row2, test_row3):
+    print("GN")
+    distance1 = list()  # []
+    distance2 = list()  # []
+    distance3 = list()  # []
+    data1 = []
+    data2 = []
+    data3 = []
+    for i in train:
+        dist1 = Euclidean_distance(test_row1, i)
+        dist2 = Euclidean_distance(test_row2, i)
+        dist3 = Euclidean_distance(test_row3, i)
+        # print("dist1 = " + str(dist1) + " dist2 = " + str(dist2))
+        if (dist1 < dist2 and dist1 < dist3):
+            distance1.append(dist1)
+            data1.append(i)
+        elif (dist3<dist2):
+            distance3.append(dist3)
+            data3.append(i)
+        else:
+            distance2.append(dist2)
+            data2.append(i)
+    if len(data1) == 0:
+        print("data1 0")
+        return
+    if len(data2) == 0:
+        print("data2 0")
+        return
+    if len(data3) == 0:
+        print("data3 0")
+        return
+    global test_r_1
+    global test_r_2
+    global test_r_3
+    test_r_1 = get_row_val(data1)
+    test_r_2 = get_row_val(data2)
+    test_r_3 = get_row_val(data3)
+    diff1 = abs(test_r_1[0] - test_row1[0]) + abs(test_r_1[1] - test_row1[1]) + abs(test_r_1[2] - test_row1[2])
+    diff2 = abs(test_r_2[0] - test_row2[0]) + abs(test_r_2[1] - test_row2[1]) + abs(test_r_2[2] - test_row2[2])
+    diff3 = abs(test_r_3[0] - test_row3[0]) + abs(test_r_3[1] - test_row3[1]) + abs(test_r_3[2] - test_row3[2])
+    if diff1 < 1 and diff2 < 1 and diff3<1:
+        print("diff < 1")
+        return
+    print("End")
+    GetTestAreas3(train, test_r_1, test_r_2, test_r_3)
+    # print(data1)
+    # print(data2)
+
 
 
 def Get_Neighbors(train, test_row, num):
@@ -120,7 +172,7 @@ def update_image(test_row1, test_row2):
             rgb.append(Blue)
             dist1 = Euclidean_distance(test_row1, rgb)
             dist2 = Euclidean_distance(test_row2, rgb)
-            if dist1< dist2:
+            if dist1 < dist2:
                 pix[x, y] = (240, 140, 40)
             else:
                 pix[x, y] = (40, 140, 240)
@@ -128,6 +180,35 @@ def update_image(test_row1, test_row2):
     im.save("knn.jpg")
     im.close()
 
+def update_image3(test_row1, test_row2, test_row3):
+    im = Image.open("land.jpg")
+    pix = im.load()
+    distance1 = list()  # []
+    distance2 = list()  # []
+    data1 = []
+    data2 = []
+
+    for x in range(im_w):
+        for y in range(im_h):
+            rgb = []
+            Blue = pix[x, y][0]
+            Green = pix[x, y][1]
+            Red = pix[x, y][2]
+            rgb.append(Red)
+            rgb.append(Green)
+            rgb.append(Blue)
+            dist1 = Euclidean_distance(test_row1, rgb)
+            dist2 = Euclidean_distance(test_row2, rgb)
+            dist3 = Euclidean_distance(test_row3, rgb)
+            if dist1 < dist2 and dist1<dist3:
+                pix[x, y] = (40, 140, 240)
+            elif dist2<dist3:
+                pix[x, y] = (10,200,20)
+            else:
+                pix[x, y] = (240, 140, 40)
+
+    im.save("knn.jpg")
+    im.close()
 
 def predict_classification(train, test_row, num):
     Neighbors = GetTestAreas(train, test_row, num)
@@ -138,6 +219,7 @@ def predict_classification(train, test_row, num):
     prediction = max(Classes, key=Classes.count)
     return prediction
     '''
+
 
 if __name__ == '__main__':
     im = Image.open("land.jpg")
@@ -176,14 +258,16 @@ if __name__ == '__main__':
 
     global test_r_1
     global test_r_2
+    global test_r_3
     test_r_1 = [176, 137, 128]
     test_r_2 = [12, 27, 29]
-    prediction = predict_classification(dataset, test_r_1, test_r_2)
+    test_r_3 = [20, 150, 50]
+    GetTestAreas3(dataset, test_r_1, test_r_2, test_r_3)
     print(test_r_1)
     print(test_r_2)
-    #print("We expected {}, Got {}".format(dataset[-1][-1], prediction))
+    # print("We expected {}, Got {}".format(dataset[-1][-1], prediction))
 
-    update_image(test_r_1, test_r_2)
+    update_image3(test_r_1, test_r_2, test_r_3)
     pass
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
